784f4846e9f2fad6e468626a5427bdc30f57da83
Dynamic Deploy <dynamicdeploy@gmail.com>
2013-11-21 10:55:03 -0800

    First Commit

---
+++ code/9/o365singlesignon/o365singlesignon/Models/TenantDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace o365singlesignon.Models
+{
+    public class TenantDbContext : DbContext
+    {
+        public TenantDbContext()
+            : base("DefaultConnection")
+        {
+        }
+
+        public DbSet<IssuingAuthorityKey> IssuingAuthorityKeys { get; set; }
+
+        public DbSet<Tenant> Tenants { get; set; }
+    }
+}
