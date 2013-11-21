db8280822a39878d270975d6ba1a0296330e90b4
Dynamic Deploy <dynamicdeploy@gmail.com>
2013-11-21 10:45:06 -0800

    First Commit

---
+++ code/2/appsforazure/appsforazure/Default.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace appsforazure
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(User.Identity.IsAuthenticated)
+            {
+
+                Trace.Write("Authenticated");
+            }
+        }
+    }
+}
